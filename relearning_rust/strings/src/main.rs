8c760af8ddfd928019be3efb39f399270825187a
Vipershroom <undertalefan812@gmail.com>
2026-02-08 23:57:05 -0500

    Add 3 string problems

--- relearning_rust/strings/src/main.rs
+++ relearning_rust/strings/src/main.rs
@@ -1,3 +1,25 @@
+fn fizz_buzz() {
+    for i in 0..101 {
+        if i % 3 == 0 {
+            println!("Fizz");
+        }
+        else if i % 5 == 0{
+            println!("Buzz");
+        }
+    }
+}
+
+fn reverse_string(s : String) -> String {
+    s.chars().rev().collect()
+}
+
+fn count_vowels(s: String) -> usize {
+    let vowels = vec!['a','e','i','o', 'u'];
+    s.chars().filter(|c| vowels.contains(c)).count()
+
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("{}", count_vowels(String::from("Mraow")));
+    println!("{}",reverse_string(String::from("mraow")))
 }
