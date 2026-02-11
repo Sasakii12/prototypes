bb6f085fd473f74891729267dff43ea0b7330168
Vipershroom <undertalefan812@gmail.com>
2026-02-10 22:39:43 -0500

    Add day 2

--- relearning_rust/advent_of_code_2025/day2/src/main.rs
+++ relearning_rust/advent_of_code_2025/day2/src/main.rs
@@ -1,3 +1,29 @@
+use std::fs::File;
+use std::io::{self,Result, Error, BufRead, BufReader};
+use std::path::Path;
+use regex::Regex;
+
+fn parse_file() -> Result<Vec<String>> {
+    let mut prod_ids = Vec::new();
+    let file_path = Path::new("./input.txt");
+
+    let file = File::open(file_path)?;
+
+    let buf_reader = BufReader::new(file);
+
+    for line in buf_reader.lines() {
+        prod_ids.push(line.unwrap().split(",").collect());
+    }
+    Ok(prod_ids)
+}
+
+fn parse_input() {
+    let re = Regex::new(r"^(\d+)\1$").unwrap();
+}
+
+
 fn main() {
     println!("Hello, world!");
+    let prod_ids = parse_file().unwrap();
+    println!("{:?}", prod_ids)
 }
