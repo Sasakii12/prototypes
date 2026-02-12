76418bb9ba63e30eed13c7165e488b0a10d1cece
Vipershroom <undertalefan812@gmail.com>
2026-02-12 11:35:06 -0500

    Create file with error handling

--- relearning_rust/book/src/main.rs
+++ relearning_rust/book/src/main.rs
@@ -1,3 +1,18 @@
+use std::fs::File;
+use std::io::ErrorKind;
+
+
+fn open_file() {
+    let f = File::open("input.txt").unwrap_or_else(|error| {
+        if error.kind() == ErrorKind::NotFound {
+            File::create("input.txt").unwrap_or_else(|error| {
+                panic!("Failure creating file");
+            })
+        } else {
+            panic!("Error opening the file")
+        }
+    });
+}
 
 fn main() {
     let mut v = vec![100,32,57];
@@ -5,4 +20,5 @@ fn main() {
         *i += 1;        
         println!("{}", i)
     }
+    open_file();
 }
