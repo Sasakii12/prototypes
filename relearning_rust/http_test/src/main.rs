2c0f5bcd83a0705119e10d28d4a7dea202cb97ab
Vipershroom <undertalefan812@gmail.com>
2026-02-13 10:03:46 -0500

    Fixed it to actually get specific pokemon data

--- relearning_rust/http_test/src/main.rs
+++ relearning_rust/http_test/src/main.rs
@@ -10,7 +10,7 @@ async fn main() {
 
 
 async fn get_pokemon(pokemon: &str) -> Result<String, Error> {
-    let url = "https://pokeapi.co/api/v2/pokemon/";
+    let url = String::from("https://pokeapi.co/api/v2/pokemon/") + pokemon;
     let body = reqwest::get(url)
     .await?.text().await?;
     Ok(body)
