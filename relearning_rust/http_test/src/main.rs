57857f7deac22ce9e812ed21331ff87fbbf37232
Vipershroom <undertalefan812@gmail.com>
2026-02-16 00:24:27 -0500

    API calls wahoo

--- relearning_rust/http_test/src/main.rs
+++ relearning_rust/http_test/src/main.rs
@@ -1,15 +1,16 @@
+use std::io;
+
 use reqwest::{Error};
 use serde::{Deserialize, Serialize};
 use serde_json::{Value, from_str};
 
-#[tokio::main]
-async fn main() {
-    let url = "https://pokeapi.co/api/v2/pokemon/ditto";
-    let pokemon = "Pikachu";
-    let js: Value = serde_json::from_str(get_pokemon(pokemon).await.unwrap().as_str()).unwrap();
-    println!("{:?}", js);
-}
+fn input() -> String {
+    let mut buf = String::new();
+    io::stdin().read_line(&mut buf)
+    .expect("Error");
 
+    buf
+}
 
 async fn get_pokemon(pokemon: &str) -> Result<String, Error> {
     let url = String::from("https://pokeapi.co/api/v2/pokemon/") + pokemon;
@@ -17,3 +18,18 @@ async fn get_pokemon(pokemon: &str) -> Result<String, Error> {
     .await?.text().await?;
     Ok(body)
 }
+
+#[tokio::main]
+async fn main() {
+    // let url = "https://pokeapi.co/api/v2/pokemon/ditto";
+    // let pokemon = "Pikachu";
+    // let js: Value = serde_json::from_str(get_pokemon(pokemon).await.unwrap().as_str()).unwrap();
+    // println!("{:?}", js);
+    println!("Please enter a pokemon name");
+    let name = input();
+    let poke_str = get_pokemon(&name).await.unwrap();
+    let poke_json: Value = serde_json::from_str(&poke_str).unwrap();
+    let prim_type = &poke_json["types"][0]["type"]["name"];
+    println!("Your pokemon is an {} type", prim_type);
+}
+
