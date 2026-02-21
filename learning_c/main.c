98a44713efbb7b5bbc4f664981e0eaefc7e7c37c
Vipershroom <undertalefan812@gmail.com>
2026-02-21 16:20:14 -0500

    math

--- learning_c/main.c
+++ learning_c/main.c
@@ -1,32 +1,18 @@
 #include <stdio.h>
-#include <string.h>
+#include <math.h>
 
 
 int main() {
-    int age = 0;
-    float gpa = 0.0f;
-    char grade = '\0';
-    char name[30] = "";
+    int x = 9;
+    
+    float sqrx = sqrt(x);
+    int powx = pow(x, 2);
 
-    printf("Please enter your age: ");
-    scanf("%d", &age);
+    float y = 1.4;
+    int roundy = round(y);
+    int ciely = ceil(y);
 
-    printf("Please enter your gpa: ");
-    scanf("%f", &gpa);
+    // more math functions you get the idea
 
-    printf("Please enter your grade: ");
-    scanf(" %c", &grade);
-
-    getchar();
-    printf("Please enter your name: ");
-    fgets(name, sizeof(name), stdin);
-    name[strlen(name) - 1] = '\0';
-
-    printf("%s\n", name);
-    printf("%d\n", age);
-    printf("%.2f\n", gpa);
-    printf("%c\n", grade);
-    printf("%s\n", name);
-
-    printf("You are %d years old\n", age);
+    printf("%d\n", ciely);
 }
