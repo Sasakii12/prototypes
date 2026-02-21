bc7d41c7d01ad4bf0fe7a4f978a95b8e158f8ccf
Vipershroom <undertalefan812@gmail.com>
2026-02-20 20:39:29 -0500

    wtf is this language man

--- learning_c/main.c
+++ learning_c/main.c
@@ -1,8 +1,32 @@
 #include <stdio.h>
-
+#include <string.h>
 
 
 int main() {
-    int age = 25;
+    int age = 0;
+    float gpa = 0.0f;
+    char grade = '\0';
+    char name[30] = "";
+
+    printf("Please enter your age: ");
+    scanf("%d", &age);
+
+    printf("Please enter your gpa: ");
+    scanf("%f", &gpa);
+
+    printf("Please enter your grade: ");
+    scanf(" %c", &grade);
+
+    getchar();
+    printf("Please enter your name: ");
+    fgets(name, sizeof(name), stdin);
+    name[strlen(name) - 1] = '\0';
+
+    printf("%s\n", name);
+    printf("%d\n", age);
+    printf("%.2f\n", gpa);
+    printf("%c\n", grade);
+    printf("%s\n", name);
+
     printf("You are %d years old\n", age);
 }
