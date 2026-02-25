98bc25286a7380d8ca6b74a2d72b53ce728434e8
Vipershroom <undertalefan812@gmail.com>
2026-02-24 19:33:26 -0500

    hello window

---
+++ relearning_rust/learn_wgpu/src/main.rs
@@ -0,0 +1,16 @@
+use glfw::{Action, Context, Key, fail_on_errors};
+
+fn main() {
+    
+    let mut glfw = glfw::init(fail_on_errors!()).unwrap();
+
+    let (mut window, events) = glfw.create_window(
+        800, 600, "Its WGPU time", glfw::WindowMode::Windowed).unwrap();
+
+    window.make_current();
+
+    while !window.should_close() {
+        glfw.poll_events();
+        window.swap_buffers();
+    }
+}
