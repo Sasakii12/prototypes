537052abb37513b08c68f934bad1d21ac8757fa4
Vipershroom <undertalefan812@gmail.com>
2026-02-24 19:59:42 -0500

    Add key polling

--- relearning_rust/learn_wgpu/src/main.rs
+++ relearning_rust/learn_wgpu/src/main.rs
@@ -7,10 +7,23 @@ fn main() {
     let (mut window, events) = glfw.create_window(
         800, 600, "Its WGPU time", glfw::WindowMode::Windowed).unwrap();
 
+        window.set_key_polling(true);
+
     window.make_current();
 
     while !window.should_close() {
         glfw.poll_events();
+        for (_, event) in glfw::flush_messages(&events) {
+            match event {
+                glfw::WindowEvent::Key(Key::Escape, _,Action::Press ,_ ) => {
+                    window.set_should_close(true);
+                }
+
+                e => {
+                    println!("{:?}", e)
+                }
+            }
+        }
         window.swap_buffers();
     }
 }
