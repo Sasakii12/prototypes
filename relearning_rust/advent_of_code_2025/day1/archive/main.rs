0354283ce4d85e4d1131dca1fbdc13de1dab79ff
Vipershroom <undertalefan812@gmail.com>
2026-03-10 11:43:58 -0400

    attempting day1 again

---
+++ relearning_rust/advent_of_code_2025/day1/archive/main.rs
@@ -0,0 +1,136 @@
+use std::fs::File;
+use std::io::{self,Result, Error, BufRead, BufReader};
+use std::path::Path;
+
+
+#[derive(Debug, Copy, Clone)]
+enum Rotation {
+    L(u32),
+    R(u32)
+}
+
+fn parse_dial(s: String) -> Rotation {
+    let a = s.split_at(1);
+    let dial = match a.0 {
+        "L" => Rotation::L(a.1.parse().unwrap()),
+        "R" => Rotation::R(a.1.parse().unwrap()),
+        _ => panic!("Dial not formatted correctly")
+    };
+    dial
+}
+
+fn rotate_dial(dial: u32,rot : Rotation) -> i32 {
+    match rot {
+        Rotation::L(num) => {
+            let temp = dial as i32 - num as i32;
+            if temp < 0 {
+                temp.rem_euclid(100) 
+            } else {
+                temp
+            }
+        }
+        Rotation::R(num) => {
+            let temp = dial as i32 + num as i32 ;
+            if temp > 99 {
+                temp % 100
+            } else {
+                temp
+            }
+        }
+    }
+}
+// -> 
+fn parse_file() -> Result<Vec<String>> {
+    let mut rots = Vec::new();
+    let file_path = Path::new("./input.txt");
+
+    let file = File::open(file_path)?;
+
+    let buf_reader = BufReader::new(file);
+
+    for line in buf_reader.lines() {
+        rots.push(line.unwrap());
+    }
+    Ok(rots)
+}
+
+fn count_zeros(v : Vec<String>, curr_dial: u32) -> u32 {
+    let mut dial = curr_dial;
+    let mut zeros = 0;
+    for rots in v {
+        let rot = parse_dial(rots);
+        let n = rotate_dial(dial, rot);
+        dial = n as u32;
+        if dial == 0 {
+            zeros += 1;
+        }
+        println!("Rotation: {:?}, Dial: {}", rot, dial)
+    }
+    zeros
+}
+
+fn rotate_dial_cycles(dial: u32,rot : Rotation) -> i32 {
+    match rot {
+        // This doesn't work but I can't be bothered lol
+        Rotation::L(num) => {
+            let temp = dial as i32 - num as i32;
+            if temp < 0 && dial != 0 {
+                println!("temp: {}", temp);
+                -temp / 100 + 1
+            } else {
+                0
+            }
+        }
+        Rotation::R(num) => {
+            let temp = dial as i32 + num as i32 ;
+            if temp > 99 {
+                temp / 100
+            } else {
+                0
+            }
+        }
+    }
+}
+
+fn count_dial_zero(v : Vec<String>, curr_dial: u32) -> i32 {
+    let mut dial = curr_dial;
+    let mut zeros = 0;
+    for rots in v {
+        let rot = parse_dial(rots);
+        let n = rotate_dial_cycles(dial, rot);
+        let n2 = rotate_dial(dial, rot);
+        
+        zeros += n;
+        dial = n2 as u32;
+        
+        // dial = n as u32;
+        if dial == 0 && n == 0{
+            zeros += 1;
+        }
+        println!("Rotation: {:?}, Dial: {}", rot, dial);
+        println!("{}", n);
+    }
+    zeros
+}
+
+fn main() {
+    // let mut zeros = 0;
+    // let mut current_dial = 50;
+    let temp_inp = vec!["L68","L30","R48","L5","R60","L55","L1","L99","R14","L82", "R50", "R1000"];
+    // let p = parse_dial("L51".to_string());
+    // println!("{:?}", rotate_dial(50, p));
+
+    // for i in temp_inp {
+    //     println!("{}", i);
+    //     let rot = parse_dial(String::from(i));
+    //     let n = rotate_dial(current_dial, rot);
+    //     println!("{}", n);
+    //     current_dial = n.try_into().unwrap();
+    //     if n == 0 {
+    //         zeros += 1
+    //     }
+    // }
+    let n = parse_file().unwrap();
+    // temp_inp.into_iter().map(String::from).collect()
+    println!("{}", count_dial_zero(temp_inp.into_iter().map(String::from).collect(), 50))
+}
--- relearning_rust/advent_of_code_2025/day1/src/main.rs
+++ relearning_rust/advent_of_code_2025/day1/src/main.rs
@@ -2,44 +2,61 @@ use std::fs::File;
 use std::io::{self,Result, Error, BufRead, BufReader};
 use std::path::Path;
 
-
-#[derive(Debug, Copy, Clone)]
 enum Rotation {
     L(u32),
-    R(u32)
+    R(u32),
 }
 
-fn parse_dial(s: String) -> Rotation {
-    let a = s.split_at(1);
-    let dial = match a.0 {
-        "L" => Rotation::L(a.1.parse().unwrap()),
-        "R" => Rotation::R(a.1.parse().unwrap()),
-        _ => panic!("Dial not formatted correctly")
-    };
-    dial
+struct Dial {
+    dial: u32,
+    clicks: u32,
 }
 
-fn rotate_dial(dial: u32,rot : Rotation) -> i32 {
-    match rot {
-        Rotation::L(num) => {
-            let temp = dial as i32 - num as i32;
-            if temp < 0 {
-                temp.rem_euclid(100) 
-            } else {
-                temp
-            }
+impl Dial {
+
+    fn parse_rot(&self,rot: &str) -> Rotation {
+        let rot_split = rot.split_at(1);
+
+        match rot_split.0 {
+            "L" => Rotation::L(rot_split.1.parse().unwrap()),
+            "R" => Rotation::R(rot_split.1.parse().unwrap()),
+            _ => panic!("rotation string not formatted correctly")
+
         }
-        Rotation::R(num) => {
-            let temp = dial as i32 + num as i32 ;
-            if temp > 99 {
-                temp % 100
-            } else {
-                temp
-            }
+        
+    }
+
+    fn rotation(&mut self, rot: Rotation) {
+        match rot {
+            Rotation::L(val) => self.rotation_l(val),
+            Rotation::R(val) => self.rotation_r(val),
+        }
+    }
+
+    fn rotation_r(&mut self, num: u32) {
+        let val = self.dial + num;
+        let val_mod = val.rem_euclid(100);
+        if val_mod == 0 {
+            self.clicks += 1;
+            self.dial = 0;
+        } else {
+            self.dial = val_mod
+        }
+        
+    }
+
+    fn rotation_l(&mut self, num: u32) {
+        let val = self.dial as i32 - num as i32;
+        let val_mod = val.rem_euclid(100) as u32;
+        if val_mod == 0 {
+            self.clicks += 1;
+            self.dial = 0;
+        } else {
+            self.dial = val_mod
         }
     }
 }
-// -> 
+
 fn parse_file() -> Result<Vec<String>> {
     let mut rots = Vec::new();
     let file_path = Path::new("./input.txt");
@@ -54,83 +71,15 @@ fn parse_file() -> Result<Vec<String>> {
     Ok(rots)
 }
 
-fn count_zeros(v : Vec<String>, curr_dial: u32) -> u32 {
-    let mut dial = curr_dial;
-    let mut zeros = 0;
-    for rots in v {
-        let rot = parse_dial(rots);
-        let n = rotate_dial(dial, rot);
-        dial = n as u32;
-        if dial == 0 {
-            zeros += 1;
-        }
-        println!("Rotation: {:?}, Dial: {}", rot, dial)
-    }
-    zeros
-}
-
-fn rotate_dial_cycles(dial: u32,rot : Rotation) -> i32 {
-    match rot {
-        // This doesn't work but I can't be bothered lol
-        Rotation::L(num) => {
-            let temp = dial as i32 - num as i32;
-            if temp < 0 && dial != 0 {
-                println!("temp: {}", temp);
-                -temp / 100 + 1
-            } else {
-                0
-            }
-        }
-        Rotation::R(num) => {
-            let temp = dial as i32 + num as i32 ;
-            if temp > 99 {
-                temp / 100
-            } else {
-                0
-            }
-        }
-    }
-}
-
-fn count_dial_zero(v : Vec<String>, curr_dial: u32) -> i32 {
-    let mut dial = curr_dial;
-    let mut zeros = 0;
-    for rots in v {
-        let rot = parse_dial(rots);
-        let n = rotate_dial_cycles(dial, rot);
-        let n2 = rotate_dial(dial, rot);
-        
-        zeros += n;
-        dial = n2 as u32;
-        
-        // dial = n as u32;
-        if dial == 0 && n == 0{
-            zeros += 1;
-        }
-        println!("Rotation: {:?}, Dial: {}", rot, dial);
-        println!("{}", n);
-    }
-    zeros
-}
-
 fn main() {
-    // let mut zeros = 0;
-    // let mut current_dial = 50;
-    let temp_inp = vec!["L68","L30","R48","L5","R60","L55","L1","L99","R14","L82", "R50", "R1000"];
-    // let p = parse_dial("L51".to_string());
-    // println!("{:?}", rotate_dial(50, p));
-
-    // for i in temp_inp {
-    //     println!("{}", i);
-    //     let rot = parse_dial(String::from(i));
-    //     let n = rotate_dial(current_dial, rot);
-    //     println!("{}", n);
-    //     current_dial = n.try_into().unwrap();
-    //     if n == 0 {
-    //         zeros += 1
-    //     }
-    // }
-    let n = parse_file().unwrap();
-    // temp_inp.into_iter().map(String::from).collect()
-    println!("{}", count_dial_zero(temp_inp.into_iter().map(String::from).collect(), 50))
+    let temp_inp = vec!["L68","L30","R48","L5","R60","L55","L1","L99","R14","L82"];
+    let puzzle_input = parse_file().unwrap();
+    let mut dial = Dial {dial : 50, clicks : 0};
+    
+    for i in puzzle_input {
+        let rot = dial.parse_rot(&i);
+        dial.rotation(rot);
+        
+    }
+    println!("dial: {}, clicks: {}", dial.dial, dial.clicks)
 }
